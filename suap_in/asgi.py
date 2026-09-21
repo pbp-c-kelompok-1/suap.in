7a04e0f8d7658505ca26ae5e0a58765115038bee
Syahid Arkan Fashihurrohman <syh.arkan@gmail.com>
2026-09-21 19:26:26 +0700

    chore: remove scaffold boilerplate comments

--- suap_in/asgi.py
+++ suap_in/asgi.py
@@ -1,12 +1,3 @@
-"""
-ASGI config for suap_in project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
