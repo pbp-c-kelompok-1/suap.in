bdd858085a3f80b7ccd72be7f017a0e8b34a93cd
Syahid Arkan Fashihurrohman <syh.arkan@gmail.com>
2026-09-21 19:26:43 +0700

    refactor: render unfinished social and challenge pages with themed placeholder

--- challenge/views.py
+++ challenge/views.py
@@ -1,13 +1,21 @@
+from django.http import JsonResponse
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
 
 
 def challenge_list_view(request):
-    return HttpResponse("Challenge list page - coming soon")
+    return render(request, 'coming_soon.html', {
+        'title': 'Misi',
+        'icon': 'mission',
+        'description': 'Tiga misi harian otomatis berdasarkan kebiasaan makanmu akan muncul di sini.',
+    })
 
 
 def weekly_report_view(request):
-    return HttpResponse("Weekly report page - coming soon")
+    return render(request, 'coming_soon.html', {
+        'title': 'Ringkasan Mingguan',
+        'icon': 'log',
+        'description': 'Rekap CO₂, perbandingan minggu lalu, dan proyeksi tahunan.',
+    })
 
 
 def api_daily_challenges(request):
